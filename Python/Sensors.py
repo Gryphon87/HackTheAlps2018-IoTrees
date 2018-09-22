e6d883908abd977de3f1820cb3ce0b31fc44cb50
unknown <filippo.bozzola@gmail.com>
2018-09-22 11:40:58 +0200

    Bugfix

--- Python/Sensors.py
+++ Python/Sensors.py
@@ -14,7 +14,7 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, gpio)
 
 # Reading the DHT11 is very sensitive to timings and occasionally
 # the Pi might fail to get a valid reading. So check if readings are valid.
-print ('Time: ' + datetime.datetime.now())
+print ('Time: {}'.format(datetime.datetime.now()))
 if humidity is not None and temperature is not None:
     print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
 else:
