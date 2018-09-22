fdbdbd1d22bdf4653548869cf0e8f908236750f6
unknown <filippo.bozzola@gmail.com>
2018-09-22 13:33:40 +0200

    bugfix

--- Python/Sensors.py
+++ Python/Sensors.py
@@ -2,7 +2,7 @@ import sys
 import Adafruit_DHT
 import datetime as dt
 import json
-import time
+import time import sleep
 import io
 
 # Set sensor type : Options are DHT11,DHT22 or AM2302
@@ -24,4 +24,4 @@ while True:
         Status= 'Ok'
 
     #print (json.dumps({'Beacon': beaconid, 'Status': Status, 'Time': time, 'Temp': temp, 'Hum': hum}))
-    time.sleep(30) #Wait 30 seconds between measurements
+    sleep(30) #Wait 30 seconds between measurements
