47eb171ed58a7e368d630b66180a2829acbc5d7c
unknown <filippo.bozzola@gmail.com>
2018-09-22 13:27:04 +0200

    BugFix

--- Python/Sensors.py
+++ Python/Sensors.py
@@ -3,20 +3,25 @@ import Adafruit_DHT
 from datetime import datetime
 import json
 import time
+import io
 
 # Set sensor type : Options are DHT11,DHT22 or AM2302
 sensor = Adafruit_DHT.DHT11
 
 # Set GPIO sensor is connected to
-gpio=17
+gpio = 17
+
+#BeaconId
+beaconid = 1
 
 while True:
     hum, temp = Adafruit_DHT.read_retry(11, 17)
     time = datetime.now()
     print('Time: {}, Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format( time, temp, hum))
     if temp is not None and hum is not None:
-        out = json.dump({'Beacon': 1, 'Status': 'Ok' 'Time': time, 'Temp': temp, 'Hum': hum})
+        Status= 'Ok'
     else:
-        out = json.dump({'Beacon': 1, 'Status': 'Err' 'Time': time, 'Temp': temp, 'Hum': hum})
+        Status= 'Ok'
+
+    print (json.dump({'Beacon': beaconid, 'Status': Status, 'Time': time, 'Temp': temp, 'Hum': hum}))
     time.sleep(30) #Wait 30 seconds between measurements
-    print (out)
