4ac112ac72d1a635cf5aa046b19a4df94b6454e1
unknown <filippo.bozzola@gmail.com>
2018-09-22 13:18:02 +0200

    BugFix

--- Python/Sensors.py
+++ Python/Sensors.py
@@ -15,7 +15,8 @@ while True:
     time = datetime.now()
     print('Time: {}, Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format( time, temp, hum))
     if temp is not None and hum is not None:
-        json = json.dump('Beacon': 1, 'Status': 'Ok' 'Time': time, 'Temp': temp, 'Hum': hum)
+        out = json.dump({'Beacon': 1, 'Status': 'Ok' 'Time': time, 'Temp': temp, 'Hum': hum})
     else:
-        json = json.dump('Beacon': 1, 'Status': 'Err' 'Time': time, 'Temp': temp, 'Hum': hum)
+        out = json.dump({'Beacon': 1, 'Status': 'Err' 'Time': time, 'Temp': temp, 'Hum': hum})
     time.sleep(30) #Wait 30 seconds between measurements
+    print (out)
