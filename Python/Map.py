310914eb56c7babb6e9ab6c61a76a1138decbb02
unknown <filippo.bozzola@gmail.com>
2018-09-22 15:22:55 +0200

    debug

--- Python/Map.py
+++ Python/Map.py
@@ -5,6 +5,6 @@ import os
 pathRaw = '{}/Data/Raw'.format(os.getcwd())
 pathProcessed = '{}/Data/Processed'.format(os.getcwd())
 
-f_list = [f for f in os.listdir(pathRaw) if os.path.isfile(f) and f.endswith('.json')]
+f_list = [f for f in os.listdir(pathRaw) if f.endswith('.json')]
 print(os.getcwd())
 print (f_list)
