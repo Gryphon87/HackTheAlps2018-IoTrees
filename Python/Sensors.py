4ac5d1d0e9699408da053180f0d628979db09d9f
unknown <filippo.bozzola@gmail.com>
2018-09-22 11:28:36 +0200

    Sensor code

---
+++ Python/Sensors.py
@@ -0,0 +1,21 @@
+import sys
+import Adafruit_DHT
+import datetime
+
+# Set sensor type : Options are DHT11,DHT22 or AM2302
+sensor=Adafruit_DHT.DHT11
+ 
+# Set GPIO sensor is connected to
+gpio=17
+
+# Use read_retry method. This will retry up to 15 times to
+# get a sensor reading (waiting 2 seconds between each retry).
+humidity, temperature = Adafruit_DHT.read_retry(sensor, gpio)
+
+# Reading the DHT11 is very sensitive to timings and occasionally
+# the Pi might fail to get a valid reading. So check if readings are valid.
+print ('Time: ' + datetime.datetime.now())
+if humidity is not None and temperature is not None:
+    print('Temp={0:0.1f}*C  Humidity={1:0.1f}%'.format(temperature, humidity))
+else:
+    print('Failed to get reading. Try again!')
